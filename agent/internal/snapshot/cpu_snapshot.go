a1dccf6b26aa6ffd1cc48e4e8a7065174dadc641
BoyJayy <popkov.ad@icloud.com>
2026-03-05 16:55:02 +0300

    Cpu snaphot reader as a func (parses form /proc/stat)

--- agent/internal/snapshot/cpu_snapshot.go
+++ agent/internal/snapshot/cpu_snapshot.go
@@ -1,15 +1,13 @@
 package snapshot
 
 type CpuSnapshot struct {
-	User    uint64
-	Nice    uint64
-	System  uint64
-	Idle    uint64
-	Iowait  uint64
-	Irq     uint64
-	Softirq uint64
-	Steal   uint64
-	Total   uint64
+	User    uint64 `json:"mount"`
+	Nice    uint64 `json:"nice"`
+	System  uint64 `json:"system"`
+	Idle    uint64 `json:"idle"`
+	Iowait  uint64 `json:"iowait"`
+	Irq     uint64 `json:"irq"`
+	Softirq uint64 `json:"softirq"`
+	Steal   uint64 `json:"steal"`
+	Total   uint64 `json:"total"`
 }
-
-
--- agent/internal/snapshot/snapshot.go
+++ agent/internal/snapshot/snapshot.go
@@ -1,6 +1,11 @@
 package snapshot
 
-import "time"
+import (
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
 type Snapshot struct {
 	Device_id         string
@@ -10,5 +15,78 @@ type Snapshot struct {
 	Mem_used_bytes    uint64
 }
 
-/*type Snapshot1 struct {
-}*/
+func ReadCPUSnapshot() (total CpuSnapshot, cores map[string]CpuSnapshot, err error) {
+	// здесь парс филдов
+	cores = make(map[string]CpuSnapshot)
+	proc, err := os.ReadFile("/proc/stat")
+	if err != nil {
+		return CpuSnapshot{}, nil, err
+	}
+	strs := strings.Split(string(proc), "\n")
+	for i := 0; i < len(strs); i++ {
+		fields := strings.Fields(strs[i])
+		if len(fields) == 0 {
+			continue
+		}
+		name := fields[0]
+		if !strings.HasPrefix(name, "cpu") {
+			continue
+		}
+		if len(fields) < 9 {
+			continue
+		}
+		user, err := strconv.ParseUint(fields[1], 10, 64)
+		if err != nil {
+			return CpuSnapshot{}, nil, err
+		}
+		nice, err := strconv.ParseUint(fields[2], 10, 64)
+		if err != nil {
+			return CpuSnapshot{}, nil, err
+		}
+		system, err := strconv.ParseUint(fields[3], 10, 64)
+		if err != nil {
+			return CpuSnapshot{}, nil, err
+		}
+		idle, err := strconv.ParseUint(fields[4], 10, 64)
+		if err != nil {
+			return CpuSnapshot{}, nil, err
+		}
+		iowait, err := strconv.ParseUint(fields[5], 10, 64)
+		if err != nil {
+			return CpuSnapshot{}, nil, err
+		}
+		irq, err := strconv.ParseUint(fields[6], 10, 64)
+		if err != nil {
+			return CpuSnapshot{}, nil, err
+		}
+		softirq, err := strconv.ParseUint(fields[7], 10, 64)
+		if err != nil {
+			return CpuSnapshot{}, nil, err
+		}
+		steal, err := strconv.ParseUint(fields[8], 10, 64)
+		if err != nil {
+			return CpuSnapshot{}, nil, err
+		}
+		tot := user + nice + system + idle + iowait + irq + softirq + steal
+		snap := CpuSnapshot{
+			User:    user,
+			Nice:    nice,
+			System:  system,
+			Idle:    idle,
+			Iowait:  iowait,
+			Irq:     irq,
+			Softirq: softirq,
+			Steal:   steal,
+			Total:   tot,
+		}
+		if name == "cpu" {
+			total = snap
+			continue
+		}
+		cores[name] = snap
+	}
+	if total.Total == 0 {
+		return CpuSnapshot{}, nil, os.ErrNotExist
+	}
+	return total, cores, nil
+}
