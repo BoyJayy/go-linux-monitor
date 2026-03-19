64985aead6d917d276d6db83e5d6cfcbf7398bdf
BoyJayy <popkov.ad@icloud.com>
2026-03-20 00:08:49 +0300

    removed json tags from cpu counters cuz its local and not gonna send

--- agent/internal/collector/cpu/types.go
+++ agent/internal/collector/cpu/types.go
@@ -1,13 +1,13 @@
 package cpu
 
 type CPUCounters struct {
-	User    uint64 `json:"user"`
-	Nice    uint64 `json:"nice"`
-	System  uint64 `json:"system"`
-	Idle    uint64 `json:"idle"`
-	Iowait  uint64 `json:"iowait"`
-	Irq     uint64 `json:"irq"`
-	Softirq uint64 `json:"softirq"`
-	Steal   uint64 `json:"steal"`
-	Total   uint64 `json:"total"`
+	User    uint64
+	Nice    uint64
+	System  uint64
+	Idle    uint64
+	Iowait  uint64
+	Irq     uint64
+	Softirq uint64
+	Steal   uint64
+	Total   uint64
 }
