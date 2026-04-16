0ec34b3562a99213d7a01c9ace2fd251a0cb941a
BoyJayy <popkov.ad@icloud.com>
2026-04-16 14:36:53 +0300

    first version of server and sender THAT DOES NOT WORK yet, ignore its just a backup

--- agent/internal/sender/http.go
+++ agent/internal/sender/http.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 )
@@ -31,13 +32,15 @@ func (s *HTTPSender) Send(metrics snapshot.Metrics) error {
 		return fmt.Errorf("http request creation: %w", err)
 	}
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
 	resp, err := s.client.Do(req)
 	if err != nil {
 		return fmt.Errorf("send request: %w", err)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		respBody, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("unexpected status code: %d, body: %s", resp.StatusCode, string(respBody))
 	}
 	return nil
 }
