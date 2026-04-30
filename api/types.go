566155e17622c25dc600b177d85b63f4f30a689e
BoyJayy <popkov.ad@icloud.com>
2026-04-30 15:18:56 +0300

    json tags

--- api/types.go
+++ api/types.go
@@ -3,59 +3,59 @@ package api
 import "time"
 
 type MemoryUsage struct {
-	TotalBytes     uint64
-	AvailableBytes uint64
-	UsedBytes      uint64
-	UsedPct        float64
+	TotalBytes     uint64  `json:"total_bytes"`
+	AvailableBytes uint64  `json:"available_bytes"`
+	UsedBytes      uint64  `json:"used_bytes"`
+	UsedPct        float64 `json:"used_pct"`
 }
 
 type CPUUsage struct {
-	UserPct    float64
-	NicePct    float64
-	SystemPct  float64
-	IdlePct    float64
-	IowaitPct  float64
-	IrqPct     float64
-	SoftirqPct float64
-	StealPct   float64
-	TotalPct   float64
+	UserPct    float64 `json:"user_pct"`
+	NicePct    float64 `json:"nice_pct"`
+	SystemPct  float64 `json:"system_pct"`
+	IdlePct    float64 `json:"idle_pct"`
+	IowaitPct  float64 `json:"iowait_pct"`
+	IrqPct     float64 `json:"irq_pct"`
+	SoftirqPct float64 `json:"softirq_pct"`
+	StealPct   float64 `json:"steal_pct"`
+	TotalPct   float64 `json:"total_pct"`
 }
 
 type DiskUsage struct {
-	Mount      string
-	TotalBytes uint64
-	FreeBytes  uint64
-	UsedBytes  uint64
-	UsedPct    float64
+	Mount      string  `json:"mount"`
+	TotalBytes uint64  `json:"total_bytes"`
+	FreeBytes  uint64  `json:"free_bytes"`
+	UsedBytes  uint64  `json:"used_bytes"`
+	UsedPct    float64 `json:"used_pct"`
 }
 
 type UsageWithCores struct {
-	UserPct    float64
-	NicePct    float64
-	SystemPct  float64
-	IdlePct    float64
-	IowaitPct  float64
-	IrqPct     float64
-	SoftirqPct float64
-	StealPct   float64
-	TotalPct   float64
-	PerCorePct map[string]CPUUsage
+	UserPct    float64             `json:"user_pct"`
+	NicePct    float64             `json:"nice_pct"`
+	SystemPct  float64             `json:"system_pct"`
+	IdlePct    float64             `json:"idle_pct"`
+	IowaitPct  float64             `json:"iowait_pct"`
+	IrqPct     float64             `json:"irq_pct"`
+	SoftirqPct float64             `json:"softirq_pct"`
+	StealPct   float64             `json:"steal_pct"`
+	TotalPct   float64             `json:"total_pct"`
+	PerCorePct map[string]CPUUsage `json:"per_core_pct"`
 }
 
 type NetIfaceStat struct {
-	Name    string
-	RxBytes uint64
-	TxBytes uint64
-	RxBps   float64
-	TxBps   float64
+	Name    string  `json:"name"`
+	RxBytes uint64  `json:"rx_bytes"`
+	TxBytes uint64  `json:"tx_bytes"`
+	RxBps   float64 `json:"rx_bps"`
+	TxBps   float64 `json:"tx_bps"`
 }
 
 type NetworkUsage struct {
-	RxBytesTotal uint64
-	TxBytesTotal uint64
-	RxBpsTotal   float64
-	TxBpsTotal   float64
-	Ifaces       []NetIfaceStat
+	RxBytesTotal uint64         `json:"rx_bytes_total"`
+	TxBytesTotal uint64         `json:"tx_bytes_total"`
+	RxBpsTotal   float64        `json:"rx_bps_total"`
+	TxBpsTotal   float64        `json:"tx_bps_total"`
+	Ifaces       []NetIfaceStat `json:"ifaces"`
 }
 
 type CPUResult struct {
