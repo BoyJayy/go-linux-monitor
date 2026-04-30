537f0eb712cf830216972088d45ebe5a2d36920b
BoyJayy <popkov.ad@icloud.com>
2026-04-30 12:49:40 +0300

    pool

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"context"
+	"log"
 	"server/internal/handler"
 	"server/internal/server"
 
-	"github.com/jackc/pgx/v5"
+	//"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
 	ctx := context.Background()
-	conn, err := pgx.Connect(ctx, "postgres://postgres:537877@localhost:5432/monitoring service")
+	conn, err := pgxpool.New(ctx, "postgres://postgres:537877@localhost:5432/monitoring service")
 	if err != nil {
-		panic(err)
+		log.Fatal("failed to create postgres pool: ", err)
 	}
-	defer conn.Close(ctx)
+	defer conn.Close()
+	if err := conn.Ping(ctx); err != nil {
+		log.Fatal("failed to ping postgres: ", err)
+	}
 	//fmt.Println("successfully started db")
 	httpHandlers := handler.NewHTTPHandlers()
 	httpServer := server.NewHTTPServer(httpHandlers)
