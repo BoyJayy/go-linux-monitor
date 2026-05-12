a9d9fde3991aa05f0ca73e639772642f97a88693
BoyJayy <popkov.ad@icloud.com>
2026-05-12 23:45:23 +0300

    Device api struct

--- api/types.go
+++ api/types.go
@@ -85,3 +85,11 @@ type Metrics struct {
 	Disk      []DiskUsage    `json:"disk"`
 	Network   NetworkUsage   `json:"network"`
 }
+
+type Device struct {
+	ID          int64     `json:"id"`
+	HostID      string    `json:"host_id"`
+	FirstSeenAt time.Time `json:"first_seen_at"`
+	LastSeenAt  time.Time `json:"last_seen_at"`
+	Online      bool      `json:"online"`
+}
