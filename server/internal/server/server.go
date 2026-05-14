d9effdc76e71a1d06b2f688f639970167e9e0025
BoyJayy <popkov.ad@icloud.com>
2026-05-14 17:36:19 +0300

    graceful shutdown for server

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -1,10 +1,14 @@
 package server
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"server/internal/handler"
-
+	"syscall"
+	"time"
 	"github.com/gorilla/mux"
 )
 
@@ -24,7 +28,34 @@ func (s *HTTPServer) StartServer() {
 	router.HandleFunc("/debug/api", s.httpHandlers.HandleDebugLast).Methods("GET")
 	staticDir := "./web"
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir(staticDir)))
-	if err := http.ListenAndServe(":8080", router); err != nil {
-		log.Fatalf("Server start error: %v", err.Error())
+	httpServer := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
 	}
+	serverErr := make(chan error, 1)
+	go func() {
+		log.Println("server started on :8080")
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			serverErr <- err
+			return
+		}
+		serverErr <- nil
+	}()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	select {
+	case <-ctx.Done():
+		log.Println("shutdown signal received")
+	case err := <-serverErr:
+		if err != nil {
+			log.Fatalf("server start error: %v", err)
+		}
+		return
+	}
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := httpServer.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("server shutdown error: %v", err)
+	}
+	log.Println("server stopped")
 }
